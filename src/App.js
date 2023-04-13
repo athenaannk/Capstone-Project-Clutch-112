107bf43cee959baaf0563b7c908f637be2c9c690
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-04-13 12:19:14 -0400

    work section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About"
+import Work from "./Components/Work";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -9,6 +11,7 @@ function App() {
     <div className="App">
       <Home />
       <About/>
+      <Work/>
     
     </div>
   );
---
+++ src/Components/Work.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Search from "../Assets/search.jpg";
+import List from "../Assets/list.jpg";
+import Sale from "../Assets/sale.jpg";
+
+const Work = () => {
+  const workInfoData = [
+    {
+      image: Search,
+      title: "Search Recipes",
+      text: "Enter ingredients you have on hand to find recipes containing all or most of what you have!",
+    },
+    {
+      image: List,
+      title: "Create Shopping List",
+      text: "Found a recipe you like? Print out ingredients you need to complete the recipe!",
+    },
+    {
+      image: Sale,
+      title: "Connect to Your Local Grocery Store",
+      text: "Search your local grocery store fliers to find the ingredients you need at their cheapest prices!",
+    },
+  ];
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Work</p>
+        <h1 className="primary-heading">How It Works</h1>
+        <p className="primary-text">
+         Simple. Three. Steps. Enter ingredients you have. Create a shopping list of ingredients you need. Explore local grocery store fliers to find them at their cheapest prices!
+        </p>
+      </div>
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info" key={data.title}>
+            <div className="info-boxes-img-container">
+              <img src={data.image} alt="" />
+            </div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Work;
