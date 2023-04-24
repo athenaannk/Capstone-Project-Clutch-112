0737ea088b4eba36ee93a3147503ab5593be5dcf
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-04-23 23:39:41 -0400

    more css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About"
 import Work from "./Components/Work";
-import Footer from "./Components/Footer"
+import Footer from "./Components/Footer";
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,6 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+ 
       <Home />
       <About/>
       <Work/>
