1180e1dafbd2b3a415b3631f4c1f15250859d7fd
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-04-24 14:05:39 -0400

    more login css

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,32 +1,86 @@
-import React, { useState } from "react"
+import React from 'react';
+import Loginbackground from "../Assets/loginbackground.png"
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
 
-export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+function Login() {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
+  return (
+    <MDBContainer fluid className="logincontainer">
 
-    }
-    return (
-        <div className="auth-form-container">
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input className="login-input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
+      <MDBRow>
 
-                <label htmlFor="password">password</label>
-                <input className="login-input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="passsword" name="password"></input>
+        <MDBCol col='10' md='6'>
+          <img src={Loginbackground} class="img-fluid" alt="Sample image" />
+        </MDBCol>
 
-                <button className="login-button" type="submit">Log In</button>
-                <button className="login-button2" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register Here!</button>
+        <MDBCol col='4' md='6'>
+
+       
+
+          
+
+          <div className="divider d-flex align-items-center my-4">
+            <p className="text-center fw-bold mx-3 mb-0">Log In</p>
+          </div>
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+          <div className="d-flex justify-content-between mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+            <a href="!#">Forgot password?</a>
+          </div>
+
+          <div className='text-center text-md-start mt-4 pt-2'>
+            <MDBBtn className="loginbutton" size='lg'>Login</MDBBtn>
+            <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href="#!" className="link-danger">Register</a></p>
+          </div>
+
+        </MDBCol>
+
+      </MDBRow>
+
+ 
+
+
+     
+
+
+
+    </MDBContainer>
+  );
+}
+
+export default Login;
+// import React, { useState } from "react"
+
+// export const Login = (props) => {
+//     const [email, setEmail] = useState('');
+//     const [pass, setPass] = useState('');
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log(email);
+
+//     }
+//     return (
+//         <div className="auth-form-container">
+//             <form className="login-form" onSubmit={handleSubmit}>
+//                 <label htmlFor="email">email</label>
+//                 <input className="login-input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
+
+//                 <label htmlFor="password">password</label>
+//                 <input className="login-input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="passsword" name="password"></input>
+
+//                 <button className="login-button" type="submit">Log In</button>
+//                 <button className="login-button2" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register Here!</button>
 
 
                 
-                </form >
-        </div> 
+//                 </form >
+//         </div> 
       
-    )
-}
+//     )
+// }
 
-export default Login
+// export default Login
--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -13,7 +13,7 @@ export const Register = (props) => {
     return (
         <div className="auth-form-container" >
         <form className="register-form"onSubmit={handleSubmit}>
-            <label>Full Name</label>
+            <label>full name</label>
             <input className="register-input" value={name} name="name" id="name" placeholder="Full Name"/>
         <label htmlFor="email">email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
@@ -25,29 +25,8 @@ export const Register = (props) => {
         <button className="register-button2" onClick={()=>props.onFormSwitch('login')}>Already have an account? Log In Here!</button>
 
     </form>
-    <div className="carousel carousel-center rounded-box">
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Pizza" />
-  </div> 
-  <div className="carousel-item">
-    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" />
-  </div>
-</div>
+
+  
     </div>
     
     )
