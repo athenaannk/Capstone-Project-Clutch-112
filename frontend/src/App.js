b4c722084e0902536a723c9647bb7550e1940e72
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-04-23 23:11:06 -0400

    nav bar beginnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About"
 import Work from "./Components/Work";
+import Footer from "./Components/Footer"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About/>
       <Work/>
+      <Footer/>
     
     </div>
   );
--- frontend/src/ProviderLayer.js
+++ frontend/src/ProviderLayer.js
@@ -1,18 +1,24 @@
-import { useFirebaseApp, AuthProvider } from "reactfire";
+import { useFirebaseApp, AuthProvider, DatabaseProvider } from "reactfire";
 import App from "./App";
-
+import DataProvider from "./Context/DataProvider";
+import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 
 const ProviderLayer = () => {
 
     const app = useFirebaseApp();
+const db = getDatabase(app);
 
     const auth = getAuth(app);
     return (
+        <DataProvider>
             <AuthProvider sdk={auth}>
+                <DatabaseProvider sdk={db}>
                     <App />
+                    </DatabaseProvider>
             </AuthProvider>
+            </DataProvider>
     )
 }
 export default ProviderLayer;
