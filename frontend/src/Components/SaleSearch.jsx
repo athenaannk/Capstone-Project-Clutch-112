7a34d8f1f190eec202d55067054ae7cf31f1fd70
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-05-11 11:55:40 -0400

    changed max on kroger price return

--- frontend/src/Components/SaleSearch.jsx
+++ frontend/src/Components/SaleSearch.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import Navbar from "./Navbar";
 import Carousel from './Carousel';
-
+import Navbar from './Navbar';
 
 const clientId = `athenakostrocapstone-aed18c45fc8b908187dd767a9654ad2c1244451021264934315`;
 const clientSecret = `bmAOj8J2KtrToThHNFQ0kv605EQwHlmBQIBe7tTz`;
@@ -27,7 +26,7 @@ const SaleSearch = () => {
     //search for the products
     const getProducts = async (token) => {
       const result = await fetch(
-        `https://api.kroger.com/v1/products?filter.locationId=01400943&filter.term=${ingredient}&filter.limit=3`,
+        `https://api.kroger.com/v1/products?filter.locationId=01400943&filter.term=${ingredient}&filter.limit=5`,
         {
           method: "GET",
           headers: {
@@ -41,7 +40,7 @@ const SaleSearch = () => {
 
     //display products
     const loadProduct = async () => {
-      let token = 'eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJhdGhlbmFrb3N0cm9jYXBzdG9uZS1hZWQxOGM0NWZjOGI5MDgxODdkZDc2N2E5NjU0YWQyYzEyNDQ0NTEwMjEyNjQ5MzQzMTUiLCJleHAiOjE2ODM3NzkwMDcsImlhdCI6MTY4Mzc3NzIwMiwiaXNzIjoiYXBpLmtyb2dlci5jb20iLCJzdWIiOiJkMmQyZTY3Ni04NWYyLTU0ZmYtYjM0Ny04NDIxNDkzY2JlNmUiLCJzY29wZSI6InByb2R1Y3QuY29tcGFjdCIsImF1dGhBdCI6MTY4Mzc3NzIwNzUyNTQ0NTAyMiwiYXpwIjoiYXRoZW5ha29zdHJvY2Fwc3RvbmUtYWVkMThjNDVmYzhiOTA4MTg3ZGQ3NjdhOTY1NGFkMmMxMjQ0NDUxMDIxMjY0OTM0MzE1In0.HRyoAFg4DeXIo5j3ODJd8v-YIa5fMFOr7XrUzTXprSN4nQdPIzi_sDhLSyn7ECK45WEW6o0vedG-CHieskjwlR7xLTDAy7QalNC0s_awQjbKS9Wo0E6Ai_atMG9XmQ6FaVd427BJumzr-9tCqW2_JNg14E93WSxK_6t5Iucr0tPwKld0lkkA7JeGj4jwEaBTLSbdPDGFYu19J7BhQcFG2_LNKnIs-r9sOn8hd6CEjrf__5fM25NhEnndOB6Ast2iRKfOE_QbqUEHCR7j-Z471yf_2inCEzNHZL6l837sXNPUPmOH6rEk60NbW-vBvfaztmmLWK9C20E-xPBr0Tbg4A'
+      let token = 'eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJhdGhlbmFrb3N0cm9jYXBzdG9uZS1hZWQxOGM0NWZjOGI5MDgxODdkZDc2N2E5NjU0YWQyYzEyNDQ0NTEwMjEyNjQ5MzQzMTUiLCJleHAiOjE2ODM4MjE2OTAsImlhdCI6MTY4MzgxOTg4NSwiaXNzIjoiYXBpLmtyb2dlci5jb20iLCJzdWIiOiJkMmQyZTY3Ni04NWYyLTU0ZmYtYjM0Ny04NDIxNDkzY2JlNmUiLCJzY29wZSI6InByb2R1Y3QuY29tcGFjdCIsImF1dGhBdCI6MTY4MzgxOTg5MDU0MDI3MzA2NywiYXpwIjoiYXRoZW5ha29zdHJvY2Fwc3RvbmUtYWVkMThjNDVmYzhiOTA4MTg3ZGQ3NjdhOTY1NGFkMmMxMjQ0NDUxMDIxMjY0OTM0MzE1In0.AfmffOxw9NLkVF7NzMzpmGG2d3yPcsxWwILJXtBpSAIcxJplidZWLfBTUmOa5ddhDtOXDuCYrx1mxWTdiXvdJeDQ9aVs0UgUiw3mLiUBGucvpl96ylRII97CEykjJbTDicYcBe4X_W5cLj021qWWMeIHtxM2H1Df8pprKCtqe3zmMxVWBtWvzFjrIn10TPrrlXfWDpK85CIW9cvzI98QK8Kd2bWkxD2-bOfYtelSgziow-Qoh2toc68-LE9Y5mZiky8ct8USYAUARoRo_JRKM5lq7HuWtpdHd63vmzCDlXd6dH1o_gwH19QCJHXVeJGclL1ReR94Q_zdDmunUm7Qgw'
       let products = await getProducts(token);
       setPrices(products);
     };
@@ -61,7 +60,7 @@ const SaleSearch = () => {
           {prices && prices.length > 0 && 
           
             <div>
-              <h2 className="primary-text">Prices for {ingredient}</h2>
+              <h2 className="primary-text2">Prices for {ingredient}</h2>
               <ul className='pricelist'>
                 {prices.map((product) => (
                   <li key={product.productId}>
